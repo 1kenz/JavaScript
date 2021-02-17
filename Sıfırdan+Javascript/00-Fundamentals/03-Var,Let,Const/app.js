66e75b648b85e6caae70fd3889c8025336e7b17a
xkendx <xkendx@users.noreply.github.com>
2021-02-17 14:15:00 +0100

    feat: update

---
+++ Sıfırdan+Javascript/00-Fundamentals/03-Var,Let,Const/app.js
@@ -0,0 +1,42 @@
+// Var
+
+// var name = 'Ken';
+// console.log(name);
+
+// name = 'Den';
+
+// console.log(name);
+
+// Let
+
+// let a,b;
+
+// a = 10;
+// b = 20;
+
+// console.log(a+b);
+
+// Const
+
+// const name = 'Ken';
+
+// console.log(name);
+
+// name = 'Den'; // rise an typeError
+
+// console.log(name);
+
+// const a;
+// a = 10; // rise an syntaxError
+
+// const a = [1, 2, 3, 4, 5];
+
+// console.log(a);
+
+// a.push(6); // only change  value not reference no rise an error
+
+// const b = 10;
+
+// b = 20;  // rise an error  
+
+// console.log(a);
