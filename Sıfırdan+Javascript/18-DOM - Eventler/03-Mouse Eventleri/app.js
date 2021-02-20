dc666acb1721e9a86d075be4e07943b33972a75a
xkendx <xkendx@users.noreply.github.com>
2021-02-20 19:46:57 +0100

    feat: update

--- Sıfırdan+Javascript/18-DOM - Eventler/03-Mouse Eventleri/app.js
+++ Sıfırdan+Javascript/18-DOM - Eventler/03-Mouse Eventleri/app.js
@@ -1,31 +1,32 @@
-const cardBody = document.querySelectorAll(".card-body")[1];
+const cardBody = document.querySelectorAll('.card-body')[1];
 
-const title = document.querySelector("#tasks-title");
+const title = document.querySelector('#tasks-title');
 
 // Click Event
 // title.addEventListener("click",run);
+
 // Double Click
 // title.addEventListener("dblclick",run);
 
 // Mouse Down
 // title.addEventListener("mousedown",run);
+
 // Mouse Up
 // title.addEventListener("mouseup",run);
 
 // Mouse Over
 // title.addEventListener("mouseover",run);
-// // Mouse Out
+
+// Mouse Out
 // title.addEventListener("mouseout",run);
 
 // cardBody.addEventListener("mouseover",run);
 // cardBody.addEventListener("mouseout",run);
 
-// Mouse Enter ve Mouse Leave
+// Mouse Enter and Mouse Leave
 
 // cardBody.addEventListener("mouseenter",run);
 // cardBody.addEventListener("mouseleave",run);
-
-
 
 function run(e) {
     console.log(e.type);
