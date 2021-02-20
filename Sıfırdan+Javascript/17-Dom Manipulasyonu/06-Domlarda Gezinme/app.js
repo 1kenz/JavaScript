9a4427c04c9695f1eb96acde401882c714f3d37b
xkendx <xkendx@users.noreply.github.com>
2021-02-20 16:30:33 +0100

    feat: update

--- Sıfırdan+Javascript/17-Dom Manipulasyonu/06-Domlarda Gezinme/app.js
+++ Sıfırdan+Javascript/17-Dom Manipulasyonu/06-Domlarda Gezinme/app.js
@@ -1,10 +1,10 @@
 let value;
 
-const todoList = document.querySelector(".list-group");
-const todo = document.querySelector(".list-group-item:nth-child(2)");
-const cardrow = document.querySelector(".card.row");
+const todoList = document.querySelector('.list-group');
+const todo = document.querySelector('.list-group-item:nth-child(2)');
+const cardrow = document.querySelector('.card.row');
 
-value= todoList;
+value = todoList;
 value = todo;
 value = cardrow;
 
@@ -17,14 +17,11 @@ value = todoList.childNodes[0];
 
 value = todoList.children;
 value = todoList.children[todoList.children.length - 1];
-value = todoList.children[2].textContent = "Değişti";
-
-
+value = todoList.children[2].textContent = 'Değişti';
 
 value = cardrow;
 value = cardrow.children;
-value = cardrow.children[2].children[1].textContent = "Burası da değişti";
-
+value = cardrow.children[2].children[1].textContent = 'Burası da değişti';
 
 value = todoList;
 value = todoList.firstElementChild;
@@ -36,7 +33,6 @@ value = cardrow;
 value = cardrow.parentElement;
 value = cardrow.parentElement.parentElement;
 
-
 // Element Kardeşleri
 
 value = todo;
@@ -45,5 +41,5 @@ value = todo.previousElementSibling;
 value = todo.nextElementSibling;
 value = todo.nextElementSibling.nextElementSibling;
 
-value = todo.previousElementSibling.previousElementSibling;
+value = todo.previousElementSibling.previousElementSibling; // null
 console.log(value);
