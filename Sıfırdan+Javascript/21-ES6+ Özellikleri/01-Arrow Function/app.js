87f2adbdd10648dcd8f249f02185cf95e4179857
xkendx <xkendx@users.noreply.github.com>
2021-02-21 16:31:29 +0100

    feat: update

---
+++ Sıfırdan+Javascript/21-ES6+ Özellikleri/01-Arrow Function/app.js
@@ -0,0 +1,29 @@
+// const hello = function(){
+//     console.log("hello");
+
+// }
+
+// hello();
+
+// Arrow Function
+
+// const hello = () => {
+//     console.log("hello");
+
+// }
+// hello();
+
+// const hello = (firstName,lastName) => console.log("hello",firstName,lastName);
+
+// const hello = firstName => console.log("hello",firstName,lastName);
+
+// hello("Ken","Den");
+
+// const cube = function(x){
+//     return x * x * x;
+// }
+// console.log(cube(4));
+
+const cube = (x) => x * x * x;
+
+console.log(cube(4));
