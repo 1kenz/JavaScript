506546df751b19a75000a9a02a138ba545c43371
xkendx <63884610+xkendx@users.noreply.github.com>
2021-06-30 20:03:58 +0300

    feat: add dom action

--- learning/Javascript/17-Dom Manipulasyonu/03-Document Object - 3/app.js
+++ learning/Javascript/17-Dom Manipulasyonu/03-Document Object - 3/app.js
@@ -11,25 +11,24 @@ value = document.scripts[0];
 
 // Linkler
 
-value = document.links;  // all a tags
+value = document.links; // all a tags
 value = document.links[0];
-value = document.links[document.links.length-1];
-value = document.links[document.links.length-1].getAttribute("class");
-value = document.links[document.links.length-1].getAttribute("href");
-value = document.links[document.links.length-1].className;
-value = document.links[document.links.length-1].classList;
-
+value = document.links[document.links.length - 1];
+value = document.links[document.links.length - 1].getAttribute('class');
+value = document.links[document.links.length - 1].getAttribute('href');
+value = document.links[document.links.length - 1].className;
+value = document.links[document.links.length - 1].classList;
 
 // Formlar
 
 value = document.forms;
 value = document.forms.length;
 value = document.forms[0];
-value = document.forms["form"];
+value = document.forms['form'];
 value = document.forms[0].id;
-value = document.forms[0].getAttribute("id");
+value = document.forms[0].getAttribute('id');
 value = document.forms[0].name;
-value = document.forms[0].getAttribute("name");
+value = document.forms[0].getAttribute('name');
 
 value = document.forms[0].method;
 
